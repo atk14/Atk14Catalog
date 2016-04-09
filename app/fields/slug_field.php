6d0919141afc36404be51f31fb7dbf70553314e4
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-04-09 23:07:44 +0200

    Added option "auto_slugify" to SlugField

--- app/fields/slug_field.php
+++ app/fields/slug_field.php
@@ -5,11 +5,23 @@ class SlugField extends RegexField{
 			"label" => _("Slug"),
 			"max_length" => 64,
 			"hint" => _("funny-green-mug"),
+			"auto_slugify" => true,
 		);
+
+		$this->auto_slugify = $options["auto_slugify"];
+		unset($options["auto_slugify"]);
+
 		parent::__construct('/^[a-z](|-?[a-z0-9]+)*$/',$options);
 
 		$this->update_messages(array(
 			"invalid" => _("Write something like black-cat-white-cat")
 		));
+	}
+
+	function clean($value){
+		if($this->auto_slugify){
+			$value = String4::ToObject($value)->toSlug()->toString();
+		}
+		return parent::clean($value);
 	}
 }
