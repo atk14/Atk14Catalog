cc262f809a12b3806fcc3d1a9472b7ebf189e2b7
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-10-21 10:42:18 +0200

    Admin script fixed
    ... it was little bit broken after the merging with the Skelet

--- public/admin/scripts/application.js
+++ public/admin/scripts/application.js
@@ -8,6 +8,8 @@
 
 			// Application-wide code.
 			init: function() {
+				ADMIN.utils.handleSortables();
+				ADMIN.utils.handleSuggestions();
 
 				// Form hints.
 				$( ".help-hint" ).each( function() {
@@ -36,7 +38,158 @@
 			}
 		},
 
+		cards: {
+			create_new: function() {
+				ADMIN.utils.tagsSuggest( "#id_tags" );
+			},
+			edit: function() {
+				ADMIN.utils.categoriesSuggest( "#id_category" );
+				ADMIN.utils.handleCardToCategories();
+				ADMIN.utils.handleCardImagesUpload();
+				ADMIN.utils.tagsSuggest( "#id_tags" );
+			}
+		},
+
+		categories: {
+			create_alias: function() {
+				ADMIN.utils.categoriesSuggest( "#id_parent_category_id" );
+			},
+			move_to_category: function() {
+				ADMIN.utils.categoriesSuggest( "#id_parent_category_id" );
+			}
+		},
+
 		utils: {
+			handleCardImagesUpload: function() {
+				var $link = $( "#imageToCard" ).hide(),
+					url = $link.attr( "href" ),
+					$progress = $( ".progress-bar" ),
+					$msg = $( ".img-message" ),
+					$list = $( ".list-group-images" ),
+					$input = $( "<input>", {
+						"id": "fileupload",
+						"type": "file",
+						"name": "files[]",
+						"data-url": url,
+						"multiple": "multiple"
+					} );
+
+				$input.insertBefore( $link );
+
+				$input.fileupload( {
+					dataType: "json",
+					multipart: false,
+					start: function() {
+						$progress.show();
+					},
+					progressall: function( e, data ) {
+						var progress = parseInt( data.loaded / data.total * 100, 10 );
+
+						$progress.css(
+							"width",
+							progress + "%"
+						);
+					},
+					done: function( e, data ) {
+						$( data.result.image_gallery_item )
+							.addClass( "not-processed" )
+							.appendTo( $list );
+
+						$msg.remove();
+					},
+					stop: function() {
+						$list.find( ".not-processed" )
+							.prepend( "<span class='glyphicon glyphicon-align-justify'></span>" )
+							.removeClass( "not-processed" );
+
+						$progress.hide().css(
+							"width",
+							"0"
+						);
+					}
+				} );
+			},
+
+			handleFormErrors: function( errors ) {
+				$.each( errors, function( field, errorList ) {
+					var $field = $( "#id_" + field ),
+						$fg = $field.closest( ".form-group" ),
+						errorMsgs = [];
+
+					// Add proper class for error styling.
+					$fg.addClass( "has-error" );
+
+					// Prepeare error messages list.
+					errorMsgs.push( "<ul class='help-block help-block-errors'>" );
+					$.each( errorList, function( i, errorMsg ) {
+						errorMsgs.push( "<li>" + errorMsg + "</li>" );
+					} );
+					errorMsgs.push( "</ul>" );
+
+					// Insert error messages list into form.
+					$( errorMsgs.join( "" ) ).insertAfter( $field );
+				} );
+			},
+
+			clearErrorMessages: function( $form ) {
+				$form.find( ".has-error" ).removeClass( "has-error" );
+				$form.find( ".help-block-errors" ).remove();
+			},
+
+			handleCardToCategories: function() {
+				var $form = $( "#form_cards_add_to_category" ),
+					$input = $( "#id_category" ),
+					$categies = $( "#categies" );
+
+				$form.on( "ajax:success", function( jqEv, json ) {
+					ADMIN.utils.clearErrorMessages( $form );
+					$categies.html( json.snippet );
+
+					if ( !json.hasErrors ) {
+						$input.val( "" );
+					} else {
+						ADMIN.utils.handleFormErrors( json.errors );
+					}
+
+					$input.focus().select();
+					ADMIN.utils.categoriesSuggest( "#id_category" );
+				} );
+			},
+
+			handleSortables: function() {
+
+				// Sortable lists.
+				var $sortable = $( ".list-sortable" ),
+					glyph = "<span class='glyphicon glyphicon-align-justify'></span>",
+					url, $item, data;
+
+				if ( $sortable.length ) {
+					$sortable.children( "li" ).prepend( glyph );
+
+					$sortable.sortable( {
+						cancel: "strong",
+						update: function( jqEv, ui ) {
+							$item = $( ui.item );
+							url = $item.closest( ".list-sortable" ).data( "sortable-url" );
+							data = {
+								id: $item.data( "id" ),
+								rank: $item.index()
+							};
+
+							$.ajax( {
+								type: "POST",
+								url: url,
+								data: data,
+								success: function() {
+								},
+								error: function() {
+								}
+							} );
+						}
+					} );
+				}
+			},
+
 			tagsSuggest: function( selector ) {
 				var $input = $( selector ),
 					lang = $( "html" ).attr( "lang" ),
@@ -86,6 +239,64 @@
 						terms.push( "" );
 						this.value = terms.join( " , " );
 						return false;
+					}
+				} );
+			},
+
+			categoriesSuggest: function( selector ) {
+				var $input = $( selector ),
+					url = $input.data( "suggest_url" ),
+					cache = {},
+					term;
+
+				if ( !$input.length ) {
+					return;
+				}
+
+				$input.autocomplete( {
+					minLength: 1,
+					source: function( request, response ) {
+						term = request.term;
+
+						if ( term in cache ) {
+							response( cache[ term ] );
+						} else {
+							$.getJSON( url + term, function( data ) {
+								cache[ term ] = data;
+								response( data );
+							} );
+						}
+					},
+					search: function() {
+						term = this.value;
+
+						if ( term.length < 1 ) {
+							return false;
+						}
+					},
+					focus: function() {
+						return false;
+					},
+					select: function( event, ui ) {
+						this.value = ui.item.value;
+						return false;
+					}
+				} );
+			},
+
+			handleSuggestions: function() {
+
+				// Naseptavani cehokoli
+				$( "[data-suggesting='yes']" ).autocomplete( {
+					source: function( request, response ) {
+						var $el = this.element,
+							url = $el.data( "suggesting_url" ),
+							term;
+						term = request.term;
+
+						$.getJSON( url, { q: term }, function( data ) {
+							response( data );
+						} );
 					}
 				} );
 			}
