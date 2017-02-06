a27870237fd1ffae281d05455eaece25e13e23c3
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-02-06 16:22:10 +0100

    CatalogId of an existing product can be updated, fixes #22

--- app/forms/admin/products/edit_form.php
+++ app/forms/admin/products/edit_form.php
@@ -1,12 +1,3 @@
 <?php
-class EditForm extends AdminForm {
-	function set_up() {
-		$this->add_translatable_field("name", new CharField(array(
-			"label" => _("Název"),
-		)));
-		$this->add_translatable_field("shortinfo", new MarkdownField(array(
-			"label" => _("Krátký popis"),
-			"required" => false,
-		)));
-	}
+class EditForm extends ProductsForm {
 }
