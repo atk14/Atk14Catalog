f30b1050661e9c49b390804df2f83dff9e7ecb49
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-07-05 16:27:21 +0200

    Changing images in migrations

--- db/migrations/0016_fulfilling_categories_migration.php
+++ db/migrations/0016_fulfilling_categories_migration.php
@@ -5,26 +5,36 @@ class FulfillingCategoriesMigration extends ApplicationMigration{
 ---
 - en: Toys, Children & Baby | Extraordinary toys for your extraordinary child
   cs: Hračky, děti a miminka | Mimořádné hračky pro vaše mimořádné dítě
+  image_url: http://i.pupiq.net/i/65/65/a40/1a40/1489x1289/Tfmb1G_800x693_0aab5602365b4819.jpg
   sub:
   - en: Electronic Toys
     cs: Elektronické hračky
+    image_url: http://i.pupiq.net/i/65/65/a42/1a42/1500x1500/iqurhB_800x800_d1e196917fd9607f.jpg
   - en: Baby & Toddler Toys
     cs: Hračky pro batolata
+    image_url: http://i.pupiq.net/i/65/65/a43/1a43/800x800/WTD4Bl_800x800_04a846b1317b4c91.jpg
   - en: Model Vehicles 
     cs: Modely vozidel
+    image_url: http://i.pupiq.net/i/65/65/a44/1a44/440x318/6hmU7k_440x318_1672a07b4848891f.jpg
 - en: Books | Reading books is just fine. Here we offer some great books.
   cs: Knihy | Čtení knih je prostě bezva. Tady nabízíme několik skvělých knih.
+  image_url: http://i.pupiq.net/i/65/65/a41/1a41/500x434/jVDLvb_500x434_cbc06e116526b779.jpg
   sub:
   - en: Food & Drink 
     cs: Jídlo a nápoje
+    image_url: http://i.pupiq.net/i/65/65/a45/1a45/733x594/n1lO92_733x594_132f9ea94c3a3e77.jpg
   - en: History
     cs: Historie
+    image_url: http://i.pupiq.net/i/65/65/a46/1a46/1024x678/BiP63Q_800x530_5e330d5f209d51aa.jpg
   - en: Computing & Internet
     cs: Počítače a Internet
+    image_url: http://i.pupiq.net/i/65/65/a47/1a47/650x329/Snvusm_650x329_304bd8558f67555a.jpg
   - en: Humour | Humour is the spice of life
     cs: Humor | Humor je koření života
+    image_url: http://i.pupiq.net/i/65/65/a48/1a48/613x440/zMO4Q9_613x440_af7b969b30a2c124.jpg
   - en: Children
     cs: Děti
+    image_url: http://i.pupiq.net/i/65/65/a49/1a49/404x297/NLEGR4_404x297_d23944dda34d3197.jpg
 ';
 
 
@@ -40,6 +50,7 @@ class FulfillingCategoriesMigration extends ApplicationMigration{
 			"name_cs" => "Knihy",
 			"parent_category_id" => $parent,
 			"pointing_to_category_id" => $category_to_pointing_to,
+      "image_url" => "http://i.pupiq.net/i/65/65/a49/1a49/404x297/NLEGR4_404x297_d23944dda34d3197.jpg"
 		));
 	}
 
@@ -72,7 +83,6 @@ class FulfillingCategoriesMigration extends ApplicationMigration{
 		}
 		$counter++;
 		$values += array(
-			"image_url" => "http://www.fillmurray.com/".(400+$counter)."/".(400+$counter),
 			"description_cs" => $lorem_ipsum,
 			"description_en" => $lorem_ipsum,
 		);
--- db/migrations/0018_fulfilling_collections_migration.php
+++ db/migrations/0018_fulfilling_collections_migration.php
@@ -11,12 +11,12 @@ class FulfillingCollectionsMigration extends ApplicationMigration{
 			"description_en" => $this->_lipsumParagraphs(),
 			"description_cs" => $this->_lipsumParagraphs(),
 
-			"image_url" => "http://www.fillmurray.com/500/500"
+			"image_url" => "http://i.pupiq.net/i/65/65/a4a/1a4a/1000x1000/mjTeHv_800x800_7d3c8bbfbf363c8d.jpg"
 		));
 		Image::CreateNewRecord(array(
 			"table_name" => "collections",
 			"record_id" => $c,
-			"url" => "http://www.fillmurray.com/800/800",
+			"url" => "http://i.pupiq.net/i/65/65/a4c/1a4c/800x465/9mRxoa_800x465_81f4477a3bf5f68e.jpg",
 
 			"name_en" => "Shaun the Sheep is everywhere",
 			"description_en" => "Shaun the Sheep is everywhere. Who doesn't believe is foolish.",
@@ -27,7 +27,7 @@ class FulfillingCollectionsMigration extends ApplicationMigration{
 		Image::CreateNewRecord(array(
 			"table_name" => "collections",
 			"record_id" => $c,
-			"url" => "http://www.fillmurray.com/g/800/800",
+			"url" => "http://i.pupiq.net/i/65/65/a4d/1a4d/1920x1280/MyJb59_800x533_3ddb82f191d55e61.jpg",
 
 			"name_en" => "People love Shaun the Sheep",
 			"description_en" => "Actually there is not any logical explanation.",
@@ -42,12 +42,12 @@ class FulfillingCollectionsMigration extends ApplicationMigration{
 			"name_cs" => "Fidorka",
 
 			"teaser_en" => "Products designed according to a popular czech chocolate biscuit",
-			"teaser_cs" => "Produkty nevržené podle oblíbeného českého čokoládového kolečka",
+			"teaser_cs" => "Produkty navržené podle oblíbeného českého čokoládového kolečka",
 
 			"description_en" => $this->_lipsumParagraphs(),
 			"description_cs" => $this->_lipsumParagraphs(),
 
-			"image_url" => "http://www.fillmurray.com/g/500/500"
+			"image_url" => "http://i.pupiq.net/i/65/65/a4b/1a4b/600x600/7RAeN8_600x600_e6821f57da5c82d4.jpg"
 		));
 	}
 }
