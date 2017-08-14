319ba7d7f2443bfac866e6990477b472deb19e16
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-08-14 16:35:02 +0200

    Tests for CatalogIdField

---
+++ test/fields/tc_catalog_id_field.php
@@ -0,0 +1,16 @@
+<?php
+class TcCatalogIdField extends TcBase {
+
+	function test(){
+		$this->field = new CatalogIdField(array());
+
+		$catalog_id = $this->assertValid("PROD123");
+		$this->assertEquals("PROD123",$catalog_id);
+
+		$catalog_id = $this->assertValid("prod123"); // since CATALOG_ID_AUTO_UPPERIZE is true
+		$this->assertEquals("PROD123",$catalog_id);
+
+		$err = $this->assertInvalid("$$%");
+		$this->assertEquals("This doesn't look like a catalog number",$err);
+	}
+}
