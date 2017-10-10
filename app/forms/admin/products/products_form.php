e8da1c8cd1e06179f5cb6dd1f80a59a7f037ccd6
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-10-10 19:01:34 +0200

    Admin tuned

--- app/forms/admin/products/products_form.php
+++ app/forms/admin/products/products_form.php
@@ -2,11 +2,11 @@
 class ProductsForm extends AdminForm {
 
 	function set_up() {
-		$this->add_field("catalog_id", new CatalogIdField(array(
-			"label" => _("Katalogové číslo"),
-		)));
 		$this->add_translatable_field("name", new CharField(array(
 			"label" => _("Název"),
+		)));
+		$this->add_field("catalog_id", new CatalogIdField(array(
+			"label" => _("Katalogové číslo"),
 		)));
 		$this->add_translatable_field("shortinfo", new MarkdownField(array(
 			"label" => _("Krátký popis"),
