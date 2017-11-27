cb011503d1a252f4d8010888682f8383f8240992
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-11-27 10:23:11 +0100

    Refactoring

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -1,23 +1,14 @@
 <?php
 class PagesController extends ApplicationController {
+
 	function detail() {
 		$this->page_title = strip_tags($this->page->getTitle());
 		$smarty = $this->_get_smarty();
-		Atk14Require::Helper("modifier.markdown.php",$smarty);
 		$this->page_description = strip_tags($this->page->getTeaser());
 
 		$this->tpl_data["child_pages"] = $this->page->getChildPages();
 
-		$pages = array($this->page);
-		$p = $this->page;
-		while($parent = $p->getParentPage()){
-			$pages[] = $parent;
-			$p = $parent;
-		}
-		$pages = array_reverse($pages);
-		foreach($pages as $p){
-			$this->breadcrumbs[] = array($p->getTitle(),$this->_link_to(array("action" => "detail", "id" => $p)));
-		}
+		$this->_add_page_to_breadcrumbs($this->page);
 	}
 
 	function _before_filter(){
