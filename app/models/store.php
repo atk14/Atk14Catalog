938ef8f0853b288de32522a70784e21fa49a83b5
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-12-29 17:54:01 +0100

    Fix

--- app/models/store.php
+++ app/models/store.php
@@ -4,7 +4,7 @@ class Store extends ApplicationModel Implements Rankable, Translatable, iSlug {
 	static function GetTranslatableFields(){ return array("name","teaser","description","address","opening_hours"); }
 
 	function setRank($rank){
-		return $this->_setRank($static);
+		return $this->_setRank($rank);
 	}
 
 	function getSlugPattern($lang){ return $this->g("name_$lang"); }
