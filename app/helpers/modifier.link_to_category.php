4da46a965dd93e0c7af60bef3239baef4d10f676
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-03-10 22:24:51 +0100

    Added helper (modifier) link_to_category

---
+++ app/helpers/modifier.link_to_category.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Builds link to a category according to the given code
+ *
+ *	<a href="{"catalog"|link_to_category}">Catalog</a>
+ */
+function smarty_modifier_link_to_category($code){
+	$category = Category::FindByCode($code);
+
+	if(!$category){
+		PRODUCTION && trigger_error("Unknown category code: $code", E_USER_WARNING);
+		return Atk14Url::BuildLink(array("namespace" => "", "controller" => "main", "action" => "page_not_found", "category" => $code));
+	}
+
+	return Atk14Url::BuildLink(array("namespace" => "", "controller" => "categories", "action" => "detail", "path" => $category->getPath()));
+}
---
+++ test/helpers/tc_link_to_category.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @fixture categories
+ */
+class TcLinkToCategory extends TcBase {
+
+	function test(){
+		global $ATK14_GLOBAL;
+
+		Atk14Require::Helper("modifier.link_to_category");
+
+		$ATK14_GLOBAL->setValue("lang","en");
+
+		$link = smarty_modifier_link_to_category("food_drinks");
+		$this->assertEquals("/catalog/food-drinks/",$link);
+
+		$link = smarty_modifier_link_to_category("weird_code");
+		$this->assertEquals("/en/main/page_not_found/?category=weird_code",$link);
+
+		$ATK14_GLOBAL->setValue("lang","cs");
+
+		$link = smarty_modifier_link_to_category("food_drinks");
+		$this->assertEquals("/katalog/jidlo-a-napoje/",$link);
+
+		$link = smarty_modifier_link_to_category("weird_code");
+		$this->assertEquals("/cs/main/page_not_found/?category=weird_code",$link);
+	}
+}
