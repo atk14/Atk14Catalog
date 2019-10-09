df34a8c6264aef8169d06100a7f4ab4a49562d68
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-09 13:19:40 +0200

    A proper page title defined for the homepage

--- db/migrations/0010_fulfilling_pages_migration.php
+++ db/migrations/0010_fulfilling_pages_migration.php
@@ -10,12 +10,12 @@ class FulfillingPagesMigration extends Atk14Migration{
 			"title_en" => "Welcome at ATK14 Catalog!",
 			"teaser_en" => "ATK14 Catalog is an skeleton suitable for applications of kind like _Products introduction_, _E-shop_, etc. ATK14 Catalog is built on top of _ATK14 Skelet_ — another great skeleton.",
 			"body_en" => "",
-			"page_title_en" => "ATK14 Skelet",
+			"page_title_en" => "ATK14 Catalog",
 
 			"title_cs" => "Vítejte v ATK14 Katalogu!",
 			"teaser_cs" => "ATK14 Katalog je skelet vhodný pro aplikace typu _produktový katalog_, _e-shop_ atd. ATK14 Katalog je postaven na _ATK14 Skeletu_ — dalším skvělém skeletu.",
 			"body_cs" => "",
-			"page_title_cs" => "ATK14 Skelet",
+			"page_title_cs" => "ATK14 Catalog",
 		));
 
 		$about = Page::CreateNewRecord(array(
