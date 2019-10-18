2185bffa10fc5361f8d2a4ea0fbc10c7f38f9557
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-18 17:52:34 +0200

    Fixes

--- app/forms/admin/products/create_new_form.php
+++ app/forms/admin/products/create_new_form.php
@@ -2,12 +2,10 @@
 class CreateNewForm extends ProductsForm {
 
 	function clean() {
-		$d = &$this->cleaned_data;
-		$product = null;
-		isset($d["catalog_id"]) && ($product = Product::FindByCatalogId($d["catalog_id"]));
-		if ($product && !$product->isDeleted()) {
-			$this->set_error("catalog_id", _("Toto katalogové číslo je již použité"));
-		}
-		return array(null, $d);
+		list($err,$d) = parent::clean();
+
+		$this->_clean_catalog_id($d);
+
+		return array($err,$d);
 	}
 }
--- app/forms/admin/products/edit_form.php
+++ app/forms/admin/products/edit_form.php
@@ -1,3 +1,11 @@
 <?php
 class EditForm extends ProductsForm {
+
+	function clean() {
+		list($err,$d) = parent::clean();
+
+		$this->_clean_catalog_id($d);
+
+		return array($err,$d);
+	}
 }
