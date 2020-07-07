7a7aad320a85b8e5106fa40dc5bb1b3584e576ee
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-07-07 12:12:12 +0200

    Action brands/index shows error404 when there are no brands

--- app/controllers/brands_controller.php
+++ app/controllers/brands_controller.php
@@ -1,8 +1,12 @@
 <?php
 class BrandsController extends ApplicationController{
+
 	function index(){
 		$this->page_title = _("List of Brands");
-		$this->tpl_data["brands"] = Brand::FindAll("visible",true);
+		$brands = $this->tpl_data["brands"] = Brand::FindAll("visible",true);
+		if(!$brands){
+			$this->_execute_action("error404");
+		}
 	}
 
 	function detail(){
