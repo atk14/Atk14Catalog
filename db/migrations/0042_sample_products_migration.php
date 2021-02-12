ac8db1f57a4611efee6723c9b288ae65fae6db36
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-02-12 14:42:26 +0100

    Added a product on the sample product card

--- db/migrations/0042_sample_products_migration.php
+++ db/migrations/0042_sample_products_migration.php
@@ -109,5 +109,11 @@ The story Bastian reads is set in the magical land of Fantastica, an unrealistic
 		));
 
 		Image::AddImage($section,"http://i.pupiq.net/i/65/65/a58/1a58/375x500/Tc3kLF_375x500_45f99cf7011a6c69.jpg");
+
+		// Product
+		$product = Product::CreateNewRecord(array(
+			"card_id" => $card,
+			"catalog_id" => "12345678",
+		));
 	}
 }
