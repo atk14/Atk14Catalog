40c6918319035e34e191a01f7119b53a4211615e
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-02-15 20:38:44 +0100

    PHP8.1 issue

--- app/models/technical_specification_key.php
+++ app/models/technical_specification_key.php
@@ -48,7 +48,7 @@ class TechnicalSpecificationKey extends ApplicationModel implements Translatable
 			$lang = $ATK14_GLOBAL->getLang();
 		}
 
-		if(strlen($key = $this->g("key_localized_$lang"))){
+		if(strlen($key = (string)$this->g("key_localized_$lang"))){
 			return $key;
 		}
 
