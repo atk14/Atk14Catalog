37f069ff7e4ce4afca761cfeb7f13b12fd88fe8e
Michal Vojan <michal.vojan@gmail.com>
2023-05-24 11:00:28 +0200

    added canonical urls on stores and brands pages

--- app/controllers/brands_controller.php
+++ app/controllers/brands_controller.php
@@ -7,11 +7,13 @@ class BrandsController extends ApplicationController{
 		if(!$brands){
 			$this->_execute_action("error404");
 		}
+		$this->head_tags->setCanonical($this->_build_canonical_url("brands/index"));
 	}
 
 	function detail(){
 		$this->page_title = $this->brand->getName();
 		$this->breadcrumbs[] = $this->brand->getName();
+		$this->head_tags->setCanonical($this->_build_canonical_url(["action" => "brands/detail", "id" => $this->brand]));
 	}
 
 	function _before_filter(){
